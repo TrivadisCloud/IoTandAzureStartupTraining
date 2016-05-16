a9d84188fb23bf9206cc39257ac4153ab804549d
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2016-05-16 16:57:18 +0200

    Added Text to Speech project

---
+++ 03_IntegrateSpeech/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Windows.Media.SpeechSynthesis;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Trivadis.IoT.Uwp.TalkToMe
+{
+  public sealed partial class MainPage : Page
+  {
+    private MediaElement _mediaElement;
+
+    public MainPage()
+    {
+      this.InitializeComponent();
+      _mediaElement = new MediaElement();
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+      var speech = new SpeechSynthesizer();
+
+      var stream = await speech.SynthesizeTextToStreamAsync(txtInput.Text);
+      _mediaElement.SetSource(stream, stream.ContentType);
+      _mediaElement.Play();
+    }
+  }
+}
